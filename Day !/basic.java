203f22b68a95c15cae2d232bf51cce62eca48b7b
altafshaikh7 <altafshaikh07781@gmail.com>
2026-03-31 20:50:07 +0530

    basic info java

---
+++ Day !/basic.java
@@ -0,0 +1,39 @@
+
+public class basic {
+    public static void main(String[] args) {
+
+        // basic types of data in java
+
+        // 1. primitive data types
+        // 2. non-primitive data types
+
+        // 1. primitive data types
+        // a. byte
+        // byte age = 25;
+        // b. short
+        // short salary = 5000;
+        // c. int
+        // int id = 1;
+        // d. long
+        // long accountNumber = 1234567890L;
+        // e. float
+        // float price = 19.99f;
+        // f. double
+        // double balance = 1000.00;
+        // g. char
+        // char grade = 'A';
+        // h. boolean
+        // boolean isStudent = true;
+
+        // 2. non-primitive data types
+        // a. String
+        String name = "John";
+        String lastName = "Doe";
+        System.out.println("Hello, " + name + " " + lastName + "!");
+        // java is 0 indexed language so the first character of the string is at index 0
+        // b. Arrays
+        // int[] numbers = {1, 2, 3, 4, 5
+        // };
+
+    }
+}
