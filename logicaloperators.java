f5189954aa29c6fdc7d3681150fea3875c93ba77
altafshaikh7 <altafshaikh07781@gmail.com>
2026-04-04 12:32:31 +0530

    Logical operators

---
+++ logicaloperators.java
@@ -0,0 +1,20 @@
+public class logicaloperators {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        // logical AND
+        System.out.println((a < b) && (b < c)); // true
+        System.out.println((a > b) && (b < c)); // false
+
+        // logical OR
+        System.out.println((a < b) || (b > c)); // true
+        System.out.println((a > b) || (b > c)); // false
+
+        // logical NOT
+        System.out.println(!(a < b)); // false
+        System.out.println(!(a > b)); // true
+    }
+}
