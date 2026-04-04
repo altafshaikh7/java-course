7d630d26b699910358fdfe871ae5671393703b38
altafshaikh7 <altafshaikh07781@gmail.com>
2026-04-04 12:37:17 +0530

    Assignment operators

---
+++ assignmentoperators.java
@@ -0,0 +1,19 @@
+public class assignmentoperators {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        // assignment operators
+        a = 15; // simple assignment
+        System.out.println(a);
+
+        b += 5; // addition assignment
+        System.out.println(b);
+
+        c -= 10; // subtraction assignment
+        System.out.println(c);
+    }
+
+}
