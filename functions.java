0ac9482021d614672217a9cb4420a190eba6b30b
altafshaikh7 <altafshaikh07781@gmail.com>
2026-04-09 00:07:47 +0530

    function overloading in java

--- functions.java
+++ functions.java
@@ -60,12 +60,20 @@ public class functions {
         return bionomial;
     }
 
+    // public static int sum(int a, int b) {
+    // return a + b;
+    // }
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
     public static int sum(int a, int b) {
         return a + b;
     }
 
-    public static int sum(int a, int b, int c) {
-        return a + b + c;
+    public static float sum(float a, float b) {
+        return a + b;
     }
 
     public static void main(String[] args) {
@@ -108,6 +116,9 @@ public class functions {
 
         System.out.println(sum(3, 5));
         System.out.println(sum(5, 5, 65));
+
+        System.out.println(sum(5, 2));
+        System.out.println(sum(50.5f, 5.9f));
 
     }
 }
