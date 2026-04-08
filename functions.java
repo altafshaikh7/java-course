c9bc0d29f0123562faaf09739f1cd32406a6ec10
altafshaikh7 <altafshaikh07781@gmail.com>
2026-04-09 00:01:35 +0530

    function overloading in java

--- functions.java
+++ functions.java
@@ -60,6 +60,14 @@ public class functions {
         return bionomial;
     }
 
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
     public static void main(String[] args) {
         // function / method
 
@@ -94,6 +102,12 @@ public class functions {
         // System.out.println(factorial(7));
 
         // bionomial coefficient
-        System.out.println(bionimialcoefficient(5, 2));
+        // System.out.println(bionimialcoefficient(5, 2));
+
+        // function overloading
+
+        System.out.println(sum(3, 5));
+        System.out.println(sum(5, 5, 65));
+
     }
 }
